29526b2c5dd1075f8c3f5e2e2ed363ddab7d2d83
Chris Hazell <chris.hazell@kinandcarta.com>
2022-10-05 09:28:11 +0100

    small change

--- tests/asyncExample.js
+++ tests/asyncExample.js
@@ -4,7 +4,7 @@ module.exports = {
       "file:///C:/Users/chris.hazell/Documents/AmazeRealise/Testing/Automation/asyncExample.html"
     );
     for (var i = 1; i <= 10; i++) {
-      browser.click("#add").waitForElementVisible(".item" + i, 10000);/*browser.click("#add").expect.element(".item" + i).to.be.visible;*/
+      browser.click("#add").waitForElementVisible(".item" + i, 10000);/*browser.click("#add").expect.element(".item" + i).to.be.visible; */
     }
     browser.end();
   }
